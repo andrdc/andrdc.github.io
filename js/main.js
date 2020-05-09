8b272730d55f06f2df41f05e4413ec3375840e1d
Andres Ruiz <andrsruiz@gmail.com>
2020-05-09 11:46:04 -0500

    Add Dynamic Divs

--- js/main.js
+++ js/main.js
@@ -43,14 +43,73 @@ webAlc = new Project(
 	"web"
 );
 
+cppTyra = new Project(
+	"cppTyra",
+	"Tyra",
+	"https://andrsrz.github.io/tyra/",
+	"Veterinary Management for Linux",
+	["C++", "Qt", "PostgreSQL"],
+	"cpp"
+);
+
+cppPE = new Project(
+	"cppPE",
+	"Programming Exercises",
+	"https://andrsrz.github.io/programming-exercises",
+	"Learn C++ and improve your problem solving skill",
+	["C++"],
+	"cpp"
+);
+
+cppSDLE = new Project(
+	"cppSDLE",
+	"SDL Examples",
+	"https://andrsrz.github.io/sdl-examples/",
+	"Examples using Simple Direct Media Layer library",
+	["C++", "SDL2"],
+	"cpp"
+);
+
+javaTyra = new Project(
+	"javaTyra",
+	"Tyra Mobile",
+	"https://github.com/Andrsrz/tyra-mobile/",
+	"Tyra client for Android",
+	["Java", "Android"],
+	"java"
+);
+
+javaGraf = new Project(
+	"javaGraf",
+	"Graficas por Computadora 2D y 3D",
+	"https://github.com/Andrsrz/graficas-por-computadora-2D-3",
+	"",
+	["Java"],
+	"java"
+);
+
+javaCompPar = new Project(
+	"javaCompPar",
+	"Computaci&oacute;n Paralela",
+	"https://github.com/Andrsrz/computacion-paralela",
+	"",
+	["Java"],
+	"java"
+);
+
+pyIoT = new Project(
+	"pyIoT",
+	"Internet of Things",
+	"https://github.com/Andrsrz/tecnologias-emergente",
+	"",
+	["Python", "Java", "Raspberry Pi", "AWS"],
+	"python"
+);
+
 const WEB_PROJECTS = [webRPSGame, webEaS, webCalc, webEEMX, webAlc];
-const CPP_PROJECTS = ["https://andrsrz.github.io/tyra/",
-						"https://andrsrz.github.io/programming-exercises/",
-						"https://andrsrz.github.io/sdl-examples/"];
-const JAVA_PROJECTS = ["https://github.com/Andrsrz/tyra-mobile/"];
-const SCHOOL_PROJECTS = ["https://github.com/Andrsrz/graficas-por-computadora-2D-3",
-						"https://github.com/Andrsrz/computacion-paralela",
-						"https://github.com/Andrsrz/tecnologias-emergente"];
+const CPP_PROJECTS = [cppTyra, cppPE, cppSDLE];
+const JAVA_PROJECTS = [javaTyra];
+const SCHOOL_PROJECTS = [javaGraf, javaCompPar, pyIoT];
 const WEB_FLEX_ID = 'webProjects';
 const CPP_FLEX_ID = "cppProjects";
 const JAVA_FLEX_ID = "javaProjects";
@@ -67,17 +126,15 @@ function createDiv(whichFlex, project){
 	let title = document.createElement("h3");
 	title.innerHTML = project.name;
 	newDiv.appendChild(title);
-	let techUsed = document.createElement("p");
-	let tech = "";
 	for(let i = 0; i < project.tech.length; i++){
-		tech += project.tech[i];
+		let techUsed = document.createElement("h5");
+		techUsed.innerHMTL = project.tech[i];
+		newDiv.appendChild(techUsed);
 	}
-	techUsed.innerHMTL = tech;
-	newDiv.appendChild(techUsed);
 }
 
 function createFlex(projects, whichFlex){
-	for(let i = 0; i <= projects.length - 1; i++){
+	for(let i = 0; i < projects.length; i++){
 		createDiv(whichFlex, projects[i]);
 	}
 }
@@ -91,6 +148,6 @@ function hoverListener(){
 
 /* Populate web page dynamically */
 createFlex(WEB_PROJECTS, WEB_FLEX_ID);
-// Createflex(CPP_PROJECTS, CPP_FLEX_ID);
-// createFlex(JAVA_PROJECTS, JAVA_FLEX_ID);
-// createFlex(SCHOOL_PROJECTS, SCHOOL_FLEX_ID);
+createFlex(CPP_PROJECTS, CPP_FLEX_ID);
+createFlex(JAVA_PROJECTS, JAVA_FLEX_ID);
+createFlex(SCHOOL_PROJECTS, SCHOOL_FLEX_ID);
