9ce0e388833a4908820a58145e25de2ca3902455
Andres Ruiz <andrsruiz@gmail.com>
2020-06-27 15:59:02 -0500

    Add Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,19 @@
 <template>
 	<div id="app">
-<section class="hero is-primary is-fullheight">
-  <!-- Hero head: will stick at the top -->
-  <div class="hero-head">
-    <nav class="navbar">
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item">
-            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
-          </a>
-          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </div>
-        <div id="navbarMenuHeroA" class="navbar-menu">
-          <div class="navbar-end">
-            <a href="#Home" class="navbar-item is-active">
-              Home
-            </a>
-            <a class="navbar-item">
-              Examples
-            </a>
-            <a class="navbar-item">
-              Documentation
-            </a>
-            <span class="navbar-item">
-              <a class="button is-primary is-inverted">
-                <span class="icon">
-                  <i class="fab fa-github"></i>
-                </span>
-                <span>Download</span>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </div>
-
-  <!-- Hero content: will be in the middle -->
-  <div class="hero-body">
-    <div class="container has-text-centered">
-      <h1 class="title">
-        Title
-      </h1>
-      <h2 class="subtitle">
-        Subtitle
-      </h2>
-    </div>
-  </div>
-
-  <!-- Hero footer: will stick at the bottom -->
-  <div class="hero-foot">
-    <nav class="tabs">
-      <div class="container">
-        <ul>
-          <li class="is-active"><a>Overview</a></li>
-          <li><a>Modifiers</a></li>
-          <li><a>Grid</a></li>
-          <li><a>Elements</a></li>
-          <li><a>Components</a></li>
-          <li><a>Layout</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</section>
-
-   <b-carousel id="Home">
-        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-            <section :class="`hero is-medium is-${carousel.color}`">
-                <div class="hero-body has-text-centered">
-                    <h1 class="title">{{carousel.text}}</h1>
-                </div>
-            </section>
-        </b-carousel-item>
-    </b-carousel>
-
+		<AndrsrzHeader />
 	</div>
 </template>
 
 <script>
+import AndrsrzHeader from './components/AndrsrzHeader.vue';
+
 export default {
 	name: 'App',
 	components: {
+		AndrsrzHeader
 	},
 	data(){
 		return {
-			carousels: [
-				{ text: 'Slide 1', color: 'primary' },
-				{ text: 'Slide 2', color: 'info' },
-				{ text: 'Slide 3', color: 'success' },
-				{ text: 'Slide 4', color: 'warning' },
-				{ text: 'Slide 5', color: 'danger' }
-			]
 		}
 	}
 }
@@ -104,9 +22,10 @@ export default {
 <style lang="scss">
 // Import Bulma's core
 @import "~bulma/sass/utilities/_all";
+@import "~bulma/bulma.sass";
 
 // Set your colors
-$primary: #8c67ef;
+$primary: red;
 $primary-invert: findColorInvert($primary);
 $twitter: #4099FF;
 $twitter-invert: findColorInvert($twitter);
---
+++ src/components/AndrsrzHeader.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="site-header">
+	<section class="hero is-primary is-fullheight">
+		<div class="hero-head">
+			<nav class="navbar">
+				<div class="container">
+					<div class="navbar-brand">
+						<a class="navbar-item">
+							<span>AR</span>
+						</a>
+						<span class="navbar-burger burger" data-target="navbarMenuHeroA">
+							<span></span>
+							<span></span>
+							<span></span>
+						</span>
+					</div>
+					<div id="navbarMenuHeroA" class="navbar-menu">
+						<div class="navbar-end">
+							<a href="#Home" class="navbar-item is-active">
+								Home
+							</a>
+							<a class="navbar-item">
+								Projects
+							</a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<h1 class="title">{{ this.name }}</h1>
+				<h2 class="subtitle">{{ this.title }}</h2>
+			</div>
+		</div>
+
+		<div class="hero-foot">
+			<nav class="tabs">
+				<div class="container">
+					<ul>
+						<li><a>Github</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</section>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'AndrsrzHeader',
+	props: {
+	},
+	data(){
+		return {
+			name: 'Andrés Ruiz',
+			title: 'Software Development Engineer'
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
