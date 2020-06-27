7c1eedb6ac2e90ef967bfe7fe83017a8267df685
Andres Ruiz <andrsruiz@gmail.com>
2020-06-27 13:48:32 -0500

    Customize Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
 	<div id="app">
-<section class="hero is-primary">
+<section class="hero is-primary is-fullheight">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
-        Primary title
+        Fullheight title
       </h1>
       <h2 class="subtitle">
-        Primary subtitle
+        Fullheight subtitle
       </h2>
     </div>
   </div>
@@ -23,6 +23,37 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #8c67ef;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 
 </style>
