41cd1b0d3526fc6781473bbb67821b5cd9b98839
Andres Ruiz <andrsruiz@gmail.com>
2020-06-27 16:13:14 -0500

    Update Deploy

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:andrsrz/andrsrz.github.io.git master:gh-pages
+git push -f git@github.com:andrsrz/andrsrz.github.io.git master
 cd -
