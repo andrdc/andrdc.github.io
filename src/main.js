3f3ad63b736aa3de1c05e4d191f02c4493573282
Andres Ruiz <andrsruiz@gmail.com>
2020-11-30 16:49:32 -0600

    Add i18n

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import i18n from './i18n'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-	publicPath: '/',
-	chainWebpack: config => {
+    publicPath: '/',
+
+    chainWebpack: config => {
 		config
 		.plugin('html')
 		.tap(args => {
 			args[0].title = 'Andr&eacute;s Ruiz'
 			return args
 		})
-	}
+	},
+
+    pluginOptions: {
+      i18n: {
+        locale: 'en',
+        fallbackLocale: 'en',
+        localeDir: 'locales',
+        enableInSFC: true
+      }
+    }
 }
